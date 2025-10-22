8fe707e8ebf03400cc70ccedd188b1587690e84a
Noemí Vargas <mimi2002@estudiantec.cr>
2025-10-22 07:36:50 -0600

    Se verifica que no se loguee el mismo usuario 2 veces

--- src/screens/main_window.py
+++ src/screens/main_window.py
@@ -9,6 +9,7 @@ class main_window:
 
     signed_in = False
     needs_reset = False
+    logged_user = set() # Conjunto para llevar control de todos los usuarios que se han logueado
 
     def __init__(self, game):
         self.game = game
@@ -130,6 +131,14 @@ class main_window:
 
         user = login_player(username, password, "./src/register/GalactaDB.db")
         if user:  # login correcto
+            # Verificar si el usuario ya está logueado
+            if username in main_window.logged_user:
+                self.show_error("Este usuario ya ha iniciado sesión")
+                return
+            
+            # En caso de que no haya iniciado sesión
+            main_window.logged_user.add(username) # Guardar el nuevo usuario
+            
             self.game.change_state("OPTIONS")
         else:  # login fallido
             self.show_error("Usuario o contraseña incorrectos")
